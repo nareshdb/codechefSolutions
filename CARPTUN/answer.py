c9713f49590772a4637bd40e54b4bfc0a3d38cdc
system <nareshdb@github.com>
2018-02-17 18:56:17 +0530

    solved CARPTUN

--- CARPTUN/answer.py
+++ CARPTUN/answer.py
@@ -1,25 +1,7 @@
 t = input()
 
-class Router:
-
-	delay = 0
-	
-
-
 for x in range(0, t):
-
 	N = int(raw_input())
-
 	A = map(int, raw_input().split())
-
 	C, D, S = map(int, raw_input().split())
-
-	time = 0
-
-	cars = [(0, 0)] * C #at time 0, all are at the first tunnel. index 0 is time, index 1 is distance travelled
-
-	while(cars[0][1] != (D * N)) {
-
-
-
-	}
+	print(max(A) * (C - 1))
